2f1d2d201d2a91b925cbe1ea3b9bafee318b6d52
Lukas Ender <hello@lukasender.at>
2015-01-10 17:07:54 +0100

    introduce logging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     exphbs = require('express-handlebars'),
     http = require('http'),
-    routes = require('./routes');
+    logger = require('./utils/logging'),
 
 // Create an express instance and set a port variable
 var app = express();
@@ -22,5 +22,5 @@ app.use("/", express.static(__dirname + "/static/"));
 
 // Fire this bitch up (start our server)
 var server = http.createServer(app).listen(port, function() {
-    console.log('Express server listening on port ' + port);
+    logger.info('Express server listening on port ' + port);
 });
---
+++ utils/logging.js
@@ -0,0 +1,8 @@
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)(),
+  ]
+});
+module.exports = logger;
